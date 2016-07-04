c6deeff100776ed3afb44a0baa97a1f8479b7ddb
Samy <sam.kitano@gmail.com>
2016-07-04 13:37:46 +0100

    Updated tests

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -4,11 +4,11 @@ namespace Kitano\Tests\Repositories;
 
 use Kitano\Repository\Contracts\RepositoryInterface as Repository;
 use Kitano\Repository\Contracts\CriteriaInterface as Criteria;
-//use \PHPUnit_Framework_TestCase as TestCase;
+use \PHPUnit_Framework_TestCase as TestCase;
 use Illuminate\Database\Eloquent\Model;
 use \Mockery as m;
 
-class RepositoryTest extends PHPUnit_Framework_TestCase
+class RepositoryTest extends TestCase
 {
     protected $mock;
 
